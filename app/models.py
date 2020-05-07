169872a85a991808dcc487d449ada5a93f89d290
Budd00 <39121962+Budd00@users.noreply.github.com>
2020-05-07 11:47:47 +0800

    To be merged to master
    This branch includes all refines in wtf_apply, register && db and flask_apply. (comments, css files, html refines)
    Please check if there is any further refine to be done. If not, please merge this brach to master brach. ;P

---
+++ app/models.py
@@ -0,0 +1,58 @@
+# from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+# from hashlib import md5
+from app import db
+
+#the tables of sql and related caculations are wirtten here
+
+class User(UserMixin,db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    Is_adm = db.Column(db.Integer)
+    mark = db.Column(db.Integer)
+    answer = db.relationship('answer', backref='author', lazy='dynamic')
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    # def __repr__(self):
+    #     return '<User {}>'.format(self.username)
+
+    # def get_id(self):
+    #     return self.id
+    
+    def figure(self): 
+        return 'https://bfgblog-a.akamaihd.net/uploads/2013/11/2-1-blackjack.png'
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+    
+
+class questions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(500), unique=True)
+    stand_answer = db.Column(db.String(500), unique=True)
+    mark = db.Column(db.Integer)
+    answer = db.relationship('answer', backref='question', lazy='dynamic')
+
+    def __repr__(self):
+        return '<questions {}>'.format(self.body)
+
+
+class answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question_id=db.Column(db.Integer, db.ForeignKey('questions.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    content = db.Column(db.String(500), unique=True)    
+    def __repr__(self):
+        return '<answer {}>'.format(self.body)
+       
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
-from flask import render_template, flash, request, redirect
-from app import app
-from app.LoginForm import LoginForm
+from flask import render_template, flash, redirect, url_for, request, send_from_directory
+from flask_login import logout_user, login_user, current_user, login_required
+from app.models import User
+from app import app,db
+from app.Form import LoginForm, RegistrationForm
+from werkzeug.urls import url_parse
+
+
 
 # app = Flask(__name__)
 app.secret_key = 'SECRET KEY'
@@ -9,17 +14,52 @@ app.secret_key = 'SECRET KEY'
 def index():
     return render_template('Application.html')
 
-@app.route('/canLogin', methods = ['POST','GET'])
+@app.route('/Login', methods = ['POST','GET'])
 def login():
-    form = LoginForm()
-    if request.method == 'GET':
-        username = request.form.get("username")
-        flash(username)
+    if current_user.is_authenticated:
+        return redirect(url_for('get_test'))
+    form = LoginForm()  
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('get_test')
+        return redirect(next_page)
+    return render_template('Can_log.html', title='Sign In', form=form)
 
 
-    return render_template('Can_log.html', form = form)
 
 @app.route('/adminLogin', methods = ['GET','POST'])
 def adminLogin():
     form = LoginForm()
-    return render_template('Admin_log.html', form = form)
+    return render_template('Admin_log.html')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/test', methods=['GET', 'POST'])
+@login_required
+def get_test():
+    return render_template('test.html',title='test')
