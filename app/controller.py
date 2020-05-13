d017c7c9b7a853ca1c0e18c323da1fa91e62fe51
Zhou-Shu <54055010+Zhou-Shu@users.noreply.github.com>
2020-05-13 16:48:44 +0800

    Update controller.py

--- app/controller.py
+++ app/controller.py
@@ -1,4 +1,5 @@
 import sqlite3
+from app.models import questions
 
 # This file contains all the functions to be tested
 
@@ -10,5 +11,18 @@ def get_tags():
     cursor = c.execute("SELECT DISTINCT tag FROM questions")
     tags = []
     for row in cursor:
-        tags.append(row[0])
+        cur_tup = (row[0],row[0])
+        tags.append(cur_tup)
     return tags
+
+
+def get_questions(tag):
+    conn = sqlite3.connect('app.db')
+    print('opened database successfully')
+    c = conn.cursor()
+    sql_query = "SELECT content FROM questions WHERE tag = '" + tag + "'"
+    c.execute(sql_query)
+    questions = []
+    for row in c:
+        questions.append(row[0].split('\n'))
+    return questions
