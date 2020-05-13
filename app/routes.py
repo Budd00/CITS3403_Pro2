ccf6f33868135d90ba891c970d2ed8b0952890df
Zhou-Shu <54055010+Zhou-Shu@users.noreply.github.com>
2020-05-13 20:39:34 +0800

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request, send_from_directory
 from flask_login import logout_user, login_user, current_user, login_required
-from app.models import User, questions
+from app.models import User, questions,answer
 from app import app, db, controller
 from app.Form import LoginForm, RegistrationForm, TagForm, AnswerForm
 from werkzeug.urls import url_parse
@@ -76,15 +76,26 @@ def get_test():
 def Que(tag,index):
     index=int(index)
     form = AnswerForm()
-    ques = controller.get_questions(tag)
+    ques,ids = controller.get_questions(tag)
     limit=len(ques)
     que=ques[index]
-    if index<limit-1:
-        index += 1
-        if form.validate_on_submit():
-            answer = form.answer.data
-            return redirect(url_for('Que', tag=tag, index=str(index)))       
-    return render_template('Questions.html', title='Questions', form=form,question=que)
+    que_id=ids[index]
+    if index>=limit-1:
+        limit=0
+    if form.validate_on_submit():
+        content = form.answer.data
+        user_id = current_user.get_id()
+        mark= controller.auto_check(content,que_id)
+        flash("Answer saved!")
+        Answer = answer(question_id=que_id,user_id=user_id,content=content,mark=mark)
+        old_answer = answer.query.filter_by( question_id=que_id, user_id=user_id).first()
+        if old_answer!= None:
+            db.session.delete(old_answer)
+            db.session.commit()
+        db.session.add(Answer)
+        db.session.commit()
+           
+    return render_template('Questions.html', title='Questions', form=form,question=que,tag=tag, nextindex=str(index+1),lastindex=str(index-1),limit=limit)
     
 
 
@@ -97,4 +108,10 @@ def welcome():
 @app.route('/mark', methods=['GET', 'POST'])
 @login_required
 def mark():
-    return render_template('mark.html', title='Home')
+    return render_template('mark.html', title='mark')
+
+
+@app.route('/ending', methods=['GET', 'POST'])
+@login_required
+def ending():
+    return render_template('endingpage.html', title='End_quiz')
