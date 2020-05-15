d8f372202867ee136dbd562a739f2556cd896583
Zhou-Shu <54055010+Zhou-Shu@users.noreply.github.com>
2020-05-15 16:11:02 +0800

    Update controller.py

--- app/controller.py
+++ app/controller.py
@@ -3,6 +3,8 @@ import sqlite3
 # This file contains all the functions to be tested
 
 # get question set tags. e.g: "HTML", "JAVASCRIPT"
+
+
 def get_tags():
     #connect to database
     conn = sqlite3.connect('app.db')
@@ -11,35 +13,40 @@ def get_tags():
     cursor = c.execute("SELECT DISTINCT tag FROM questions")
     tags = []
     for row in cursor:
-        cur_tup = (row[0],row[0])
+        cur_tup = (row[0], row[0])
         tags.append(cur_tup)
     return tags
 
 # get question contents and quesiton ids
+
+
 def get_questions(tag):
     conn = sqlite3.connect('app.db')
     print('opened database successfully')
     c = conn.cursor()
-    sql_query = "SELECT content FROM questions WHERE tag = '" + tag + "'" 
+    sql_query = "SELECT content FROM questions WHERE tag = '" + tag + "'"
     c.execute(sql_query)
     questions = []
     ids = []
     for row in c:
         questions.append(row[0].split('\n'))
-    sql_query = "SELECT id FROM questions WHERE tag = '" + tag + "'" 
+    sql_query = "SELECT id FROM questions WHERE tag = '" + tag + "'"
     c.execute(sql_query)
     for row in c:
         ids.append(int(row[0]))
 
-    return questions,ids
+    return questions, ids
 
 # check if the user's answer is right and return responsing mark
 # 0 if not correct, -1 if mark in db is None, full mark if answer is right
+
+
 def auto_check(answer, que_id):
     conn = sqlite3.connect('app.db')
     print('opened database successfully')
     c = conn.cursor()
-    sql_query = "SELECT stand_answer FROM questions WHERE id = '" + str(que_id) + "'"
+    sql_query = "SELECT stand_answer FROM questions WHERE id = '" + \
+        str(que_id) + "'"
     c.execute(sql_query)
     std_ans = ''
     for row in c:
@@ -49,7 +56,8 @@ def auto_check(answer, que_id):
     c.execute(sql_query)
     for row in c:
         std_mark = row[0]
-    print('std_ans: ', std_ans, 'std_mark: ', std_mark, 'type of mark: ', type(std_mark))
+    print('std_ans: ', std_ans, 'std_mark: ',
+          std_mark, 'type of mark: ', type(std_mark))
     final_mark = 0
     # if there isn't a standard mark
     if std_mark == None:
@@ -59,3 +67,31 @@ def auto_check(answer, que_id):
         final_mark = std_mark
 
     return final_mark
+
+
+def get_mark(tag, uid):
+    conn = sqlite3.connect('app.db')
+    print('opened database successfully')
+    c = conn.cursor()
+    sql_query = "SELECT answer.mark FROM questions, User, answer where questions.id = question_id " \
+                "and questions.tag = '" + tag + "' and  User.id = " + str(uid)
+    c.execute(sql_query)
+    arr = []
+    for row in c:
+        if row[0] == -1:
+            return 'Assesment is not finished yet!',0
+        arr.append(row[0])
+    return arr,sum(arr)
+
+
+def get_question_mark(tag):
+    conn = sqlite3.connect('app.db')
+    print('opened database successfully')
+    c = conn.cursor()
+    sql_query = "SELECT mark FROM questions where questions.tag = '" + tag + "'"
+    c.execute(sql_query)
+    arr = []
+    for row in c:
+        arr.append(row[0])
+    return arr,sum(arr)
+
