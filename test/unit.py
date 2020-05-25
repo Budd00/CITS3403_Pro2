f6ed2ac456d50b405dee97a1347029348058a418
GREYXXX <33743607+GREYXXX@users.noreply.github.com>
2020-05-25 19:39:36 +0800

    Update unit.py

--- test/unit.py
+++ test/unit.py
@@ -6,9 +6,11 @@ from app.models import User, questions,answer
 class UserModelTest(unittest.TestCase):
 
     def setUp(self):
+     #Setting test_client()
         self.app = app.test_client()
         db.create_all()
         u = User.query.filter_by(username='username').first()
+     #Checking if this account alrady in User table
         if u != None:
             db.session.delete(u)
             db.session.commit()
@@ -16,6 +18,7 @@ class UserModelTest(unittest.TestCase):
         db.session.add(u1)
         line = '1: HTML stands for?'
         q = questions.query.filter_by(content = line).first()
+     #Checking if this questions already in questions table
         if q != None:
             db.session.delete(q)
             db.session.commit()
@@ -27,13 +30,14 @@ class UserModelTest(unittest.TestCase):
     def tearDown(self):
         pass
     
-    
+    #Testing if we can correctly set password
     def test_set_pw(self):
         u = User.query.get(1)
         u.set_password('password')
         self.assertFalse(u.check_password('passw0rd'))
         self.assertTrue(u.check_password('password'))
-
+        
+    #Testing if we can get correct result from questions table
     def test_guestions(self):
         line = '1: HTML stands for?'
         self.assertEqual('HTML1', questions.query.filter_by(content = line).first().tag)
